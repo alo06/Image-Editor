5d230826f64004d14b07a820e42312522542c83a
Jeevottam <bhatjeevu06@gmail.com>
2023-07-29 12:41:49 +0530

    resize

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const choose = document.querySelector("#choose"),
   filterName = document.querySelector(".filter-value .name"),
   filterValue = document.querySelector(".filter-value .value"),
   filterSlider = document.querySelector(".slider input"),
-  rotateBtn = document.querySelectorAll(".rotate button");
+  rotateBtn = document.querySelectorAll(".rotate button"),
+  width=document.querySelector('#width'),
+  height=document.querySelector('#height'),
+  aspectRatio=document.querySelector("#ratio");
 let brightness = "100",
   saturation = "100",
   inversion = "0",
@@ -16,8 +19,8 @@ let brightness = "100",
   hue="180",
   rotate = 0,
   horizontal = 1,
-  vertical = 1;
-
+  vertical = 1,
+  actualRatio;
 const loadImg = () => {
   let img = img_input.files[0];
   if (!img) {
@@ -25,6 +28,9 @@ const loadImg = () => {
   }
   previewImg.src = URL.createObjectURL(img);
   previewImg.addEventListener("load", () => {
+    width.value=previewImg.naturalWidth;
+    height.value=previewImg.naturalHeight;
+    actualRatio=previewImg.naturalWidth/previewImg.naturalHeight;
     reset.click();
   });
 };
@@ -71,6 +77,14 @@ const applyFilter = () => {
   previewImg.style.filter = `brightness(${brightness}%) saturate(${saturation}%) grayscale(${grayscale}%) invert(${inversion}%) blur(${blurr}px) hue-rotate(${hue}deg)`;
 };
 
+width.addEventListener("keyup",()=>{
+  height.value = Math.floor(aspectRatio.checked ? width.value / actualRatio : height.value);
+})
+width.addEventListener("keyup", () => {
+  width.value = Math.floor(aspectRatio.checked ? height.value * actualRatio : width.value);
+});
+
+
 rotateBtn.forEach((option) => {
   option.addEventListener("click", () => {
     console.log(option.id);
@@ -116,10 +130,28 @@ const resetFilter = () => {
     (hue="0"),
   (rotate = 0), (horizontal = 1), (vertical = 1);
   filterBtn[0].click();
+  width.value = previewImg.naturalWidth;
+  height.value = previewImg.naturalHeight;
+  actualRatio = previewImg.naturalWidth / previewImg.naturalHeight;
   applyFilter();
 };
 
-const saveImg = () => {};
+const saveImg = () => {
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = width.value;
+  canvas.height = height.value;
+  ctx.filter = `brightness(${brightness}%) saturate(${saturation}%) grayscale(${grayscale}%) invert(${inversion}%) blur(${blurr}px) hue-rotate(${hue}deg)`;
+  if (rotate !== 0) {
+    ctx.rotate((rotate * Math.PI) / 180);
+  }
+  ctx.scale(horizontal, vertical);
+  ctx.drawImage(previewImg, 0, 0, canvas.width, canvas.height);
+  const link = document.createElement("a");
+  link.download = "image.jpg";
+  link.href = canvas.toDataURL();
+  link.click();
+};
 
 img_input.addEventListener("change", loadImg);
 choose.addEventListener("click", () => img_input.click());
